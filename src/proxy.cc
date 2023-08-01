e6146a001d54a6520c277fe06536de7b715c2040
Janiece Eleonor <janiecee@un-j4.kzn.21-school.ru>
2023-08-01 12:18:08 +0300

    libeevent try #2 WORKING!

--- src/proxy.cc
+++ src/proxy.cc
@@ -1,27 +1,23 @@
+#include </opt/goinfre/janiecee/homebrew/Cellar/libevent/2.1.12_1/include/event2/event.h>  // Заголовочный файл libevent
 #include <arpa/inet.h>  // Для структуры sockaddr_in
 #include <sys/socket.h>  // Для создания сокетов, привязки и т.д.
+#include <unistd.h>
 
 #include <iostream>
-
-#include "/opt/goinfre/janiecee/homebrew/Cellar/libevent/2.1.12_1/include/event2/event.h"  // Заголовочный файл libevent
 
 void read_callback(evutil_socket_t fd, short events, void* arg) {
   char buffer[4096];
   int recv_len = recv(fd, buffer, sizeof(buffer), 0);
   if (recv_len > 0) {
-    // Обработка полученных данных
+    // Process the received data
   } else {
-    // Произошла ошибка или клиент закрыл соединение
-    event* ev = static_cast<event*>(arg);
-    event_del(ev);
-    delete ev;
+    // An error occurred or the client closed the connection
     close(fd);
   }
 }
 
 int main() {
-  int server_socket =
-      socket(AF_INET, SOCK_STREAM, 0);  // Получить дескриптор сокета сервера
+  int server_socket = socket(AF_INET, SOCK_STREAM, 0);
   if (server_socket < 0) {
     std::cerr << "Failed to create server socket." << std::endl;
     return 1;
@@ -32,7 +28,8 @@ int main() {
   server_addr.sin_addr.s_addr = INADDR_ANY;
   server_addr.sin_port = htons(8000);
 
-  if (bind(server_socket, (sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+  if (bind(server_socket, reinterpret_cast<struct sockaddr*>(&server_addr),
+           sizeof(server_addr)) < 0) {
     std::cerr << "Failed to bind server socket." << std::endl;
     close(server_socket);
     return 1;
@@ -40,7 +37,7 @@ int main() {
 
   listen(server_socket, 10);
 
-  event_base* base = event_base_new();  // Создать основу для событий libevent
+  event_base* base = event_base_new();
   if (!base) {
     std::cerr << "Failed to create event base." << std::endl;
     close(server_socket);
@@ -48,7 +45,7 @@ int main() {
   }
 
   event* ev = event_new(base, server_socket, EV_READ | EV_PERSIST,
-                        read_callback, event_self_cbarg());
+                        read_callback, nullptr);
   if (!ev) {
     std::cerr << "Failed to create event." << std::endl;
     event_base_free(base);
@@ -56,9 +53,9 @@ int main() {
     return 1;
   }
 
-  event_add(ev, nullptr);  // Добавить событие на обработку
+  event_add(ev, nullptr);
 
-  event_base_dispatch(base);  // Начать обработку событий
+  event_base_dispatch(base);
 
   event_free(ev);
   event_base_free(base);
